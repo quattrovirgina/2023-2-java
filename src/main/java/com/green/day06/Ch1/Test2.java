e0c904abbb74ba0e72ae15dc3eb563dca57b86af
first2 <gimdongha603@gmail.com>
2023-10-04 15:54:22 +0900

    dfdfdfdf

--- src/main/java/com/green/day06/Ch1/Test2.java
+++ src/main/java/com/green/day06/Ch1/Test2.java
@@ -2,6 +2,14 @@ package com.green.day06.Ch1;
 
 public class Test2 {
     public static void main(String[] args) {
-
+        int i = 0;
+        while(i < 10) {
+            System.out.println("for - " + i);
+            i++;
+            if(i == 5) {
+                System.out.println("그만해 임마");
+            }
+            continue;
+        }
     }
-}
+ }
